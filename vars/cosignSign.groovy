b091d996c709419f249c31a79887497b27cf9d1d
Jason Castonguay <jason.castonguay@leidos.com>
2026-05-27 20:35:44 +0000

    added jenkins-role, added podtemplates as yaml so they can be called in vars

--- vars/cosignSign.groovy
+++ vars/cosignSign.groovy
@@ -5,16 +5,36 @@ def call(Map config = [:]) {
         return
     }
 
-    def cosign_image = config.cosign_image ?: 'artifactory.cloud.cms.gov/docker/sigstore/cosign@sha256:c1b2d3e4f5a6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2'
-    def aws_image = config.aws_image ?: 'artifactory.cloud.cms.gov/docker/amazon/aws-cli@sha256:0b894cdaa3836d70050f293b9e993c546e222458e64e145b93a783efd24a7046'
-    def image = env.IMAGE_TAG ?: error("IMAGE_TAG not set")
+    def role_name = config.role_name ?: 'deploy-role'
+    def region = config.region ?: 'us-east-1'
+    def account_id = config.account_ids?. error
+    def role_arn = "arn:aws:iam::${account_id}:role/${role_name}"
+
+    def cosignYaml = config.cosign_yaml ?: readTrusted("resources/pods/cosign.yaml")
+    def awsYaml = config.aws_yaml ?: readTrusted("resources/pods/aws-cli.yaml")
+
+
 
     stage("Cosign Sign") {
-        podTemplate(containers: [
-            containerTemplate(name: 'cosign', image: cosign_image, command: 'cat', ttyEnabled: true),
-            containerTemplate(name: 'aws-cli', image: aws_image, command: 'cat', ttyEnabled: true)
-        ]) {
-            node(POD_LABEL) {
+        podTemplate(yaml: cosignYaml) {
+           podTemplate(yaml: awsYaml){
+             node(POD_LABEL) {
+                container('aws-cli') {
+                    def creds = sh(
+                        script: """
+                            aws sts assume-role \
+                                --role-arn ${role_arn} \
+                                --role-session-name jenkins-${env.BUILD_NUMBER} \
+                                --region ${region} \
+                                --output json
+                        """,
+                        returnStdout: true
+                    ).trim()
+                    def parsed = readJSON text: creds
+                    env.AWS_ACCESS_KEY_ID = parsed.Credentials.AccessKeyId
+                    env.AWS_SECRET_ACCESS_KEY = parsed.Credentials.SecretAccessKey
+                    env.AWS_SESSION_TOKEN = parsed.Credentials.SessionToken
+                }
                 container('cosign') {
                     sh """
                         cosign sign --key awskms:///${kms_key_arn} \
@@ -22,7 +42,8 @@ def call(Map config = [:]) {
                             ${image}
                     """
                 }
-            }
+             }
+           }
         }
     }
 }
