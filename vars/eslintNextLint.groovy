a73494742b285da92e0001372842f7d8647430bf
Jason Castonguay <jason.castonguay@leidos.com>
2026-07-02 21:56:22 +0000

    add react plugin to eslint

--- vars/eslintNextLint.groovy
+++ vars/eslintNextLint.groovy
@@ -9,7 +9,7 @@ def call(Map config = [:]) {
                 unstash "workspace"
                 container('node') {
                     sh "npm config set registry https://artifactory.cloud.cms.gov/artifactory/api/npm/npm/"
-                    sh "npm install --save-dev eslint next @eslint/js typescript-eslint eslint-config-next globals" 
+                    sh "npm install --save-dev eslint next @eslint/js typescript-eslint eslint-plugin-react eslint-config-next globals" 
                     def exit_code = sh(
                         script: "cd ${working_dir} && npx eslint ${paths} --format json --output-file eslint-results.json",
                         returnStatus: true
