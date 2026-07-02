0184235cc2cfd85fb12310696aac6fcc1255d333
Jason Castonguay <jason.castonguay@leidos.com>
2026-07-02 22:03:23 +0000

    fixing go linter errors

--- demos/go/main.go
+++ demos/go/main.go
@@ -1,3 +1,4 @@
+// Package main is the heart of this webapp
 package main
 
 import (
@@ -9,10 +10,12 @@ import (
 
 var templates *template.Template
 
+// init sets the templates
 func init() {
 	templates = template.Must(template.ParseGlob(filepath.Join("templates", "*.html")))
 }
 
+// homeHandler is for home.html
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -22,11 +25,13 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "home.html", map[string]string{"PageTitle": "Home"})
 }
 
+// aboutHandler is for about.html
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	templates.ExecuteTemplate(w, "about.html", map[string]string{"PageTitle": "About"})
 }
 
+// main sets up our handlers
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", homeHandler)
