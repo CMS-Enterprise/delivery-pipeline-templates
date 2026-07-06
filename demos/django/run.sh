0669657d396ef394660bd9aa8979c4ab0c5d3c51
Jason Castonguay <jason.castonguay@leidos.com>
2026-07-06 14:38:27 +0000

    vue update packages

--- demos/django/run.sh
+++ demos/django/run.sh
@@ -3,9 +3,9 @@ set -e
 cd "$(dirname "$0")"
 
 if [ ! -d venv ]; then
-    python3 -m venv venv
+  python3 -m venv venv
 fi
 
 source venv/bin/activate
-pip install --quiet -r requirements.txt
+pip3 install --quiet -r requirements.txt
 python manage.py runserver 3003
