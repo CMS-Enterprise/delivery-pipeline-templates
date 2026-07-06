07295917a5690d3c5c4da8d8949d82135a3117f3
Jason Castonguay <jason.castonguay@leidos.com>
2026-07-06 15:46:33 +0000

    fix static path

--- demos/django/mysite/settings.py
+++ demos/django/mysite/settings.py
@@ -63,7 +63,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / STATIC_URL
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
